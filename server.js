57fff8d85a3453836201eeaef702b64eaff4270b
InfantRajuGit <infantraju123@gmail.com>
2021-01-12 14:28:26 +0530

    voice calling feature

---
+++ server.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+var httpServer = require('http');
+
+const ioServer = require('socket.io');
+const RTCMultiConnectionServer = require('rtcmulticonnection-server');
+
+var PORT = 5000;
+var isUseHTTPs = false;
+
+const jsonPath = {
+    config: 'config.json',
+    logs: 'logs.json'
+};
+
+const BASH_COLORS_HELPER = RTCMultiConnectionServer.BASH_COLORS_HELPER;
+const getValuesFromConfigJson = RTCMultiConnectionServer.getValuesFromConfigJson;
+const getBashParameters = RTCMultiConnectionServer.getBashParameters;
+const resolveURL = RTCMultiConnectionServer.resolveURL;
+
+var config = getValuesFromConfigJson(jsonPath);
+config = getBashParameters(config, BASH_COLORS_HELPER);
+
+
+if(PORT === 5000) {
+    PORT = config.port;
+}
+if(isUseHTTPs === false) {
+    isUseHTTPs = config.isUseHTTPs;
+}
+
+function serverHandler(request, response) {
+    // to make sure we always get valid info from json file
+    // even if external codes are overriding it
+    config = getValuesFromConfigJson(jsonPath);
+    config = getBashParameters(config, BASH_COLORS_HELPER);
+
+    // HTTP_GET handling code goes below
+    try {
+        var uri, filename;
+            uri = url.parse(request.url).pathname;
+            filename = path.join(config.dirPath ? resolveURL(config.dirPath) : process.cwd(), uri);
+
+        var contentType = 'text/plain';
+        if (filename.toLowerCase().indexOf('.html') !== -1) {
+            contentType = 'text/html';
+        }
+        if (filename.toLowerCase().indexOf('.css') !== -1) {
+            contentType = 'text/css';
+        }
+        if (filename.toLowerCase().indexOf('.png') !== -1) {
+            contentType = 'image/png';
+        }
+
+        fs.readFile(filename, 'binary', function(err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+                response.end();
+                return;
+            }
+
+            try {
+                file = file.replace('connection.socketURL = \'/\';', 'connection.socketURL = \'' + config.socketURL + '\';');
+            } catch (e) {}
+
+            response.writeHead(200, {
+                'Content-Type': contentType
+            });
+            response.write(file, 'binary');
+            response.end();
+        });
+    } catch (e) {
+        pushLogs(config, 'Unexpected', e);
+
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        response.write('404 Not Found: Unexpected error.\n' + e.message + '\n\n' + e.stack);
+        response.end();
+    }
+}
+
+var httpApp;
+httpApp = httpServer.createServer(serverHandler);
+
+RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
+httpApp = httpApp.listen(process.env.PORT || PORT, process.env.IP || "0.0.0.0", function() {
+    RTCMultiConnectionServer.afterHttpListen(httpApp, config);
+});
+
+// --------------------------
+// socket.io codes goes below
+
+ioServer(httpApp).on('connection', function(socket) {
+    RTCMultiConnectionServer.addSocket(socket, config);
+
+    // ----------------------
+    // below code is optional
+
+    const params = socket.handshake.query;
+
+    if (!params.socketCustomEvent) {
+        params.socketCustomEvent = 'custom-message';
+    }
+
+    socket.on(params.socketCustomEvent, function(message) {
+        socket.broadcast.emit(params.socketCustomEvent, message);
+    });
+});
